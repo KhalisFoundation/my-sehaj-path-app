9ff74a0167f8b6ee6edd02909106a1f4876391c1
iKumarAbhinav <kumar@adaapt.ai>
2024-09-18 20:03:32 +0530

    Added font Loading functions

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { useFonts, WorkSans_400Regular, WorkSans_700Bold } from '@expo-google-fonts/work-sans';
+import ProgressCard from './App/components/ProgressCard';
+import { typography } from './App/styles/typography';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    WorkSans_400Regular,
+    WorkSans_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null; // or a loading screen
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ProgressCard sehajNumber={14} angNumber={745} progress={30} />
     </View>
   );
 }
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
