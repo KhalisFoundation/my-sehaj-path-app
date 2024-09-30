4de4b76bc158f356b021720d4a5e5dca75b19c46
Abhishek singh <abhiramgarhia13@gmail.com>
2024-09-30 23:06:07 +0530

    Links are corrected and Only baloopaaji font is working

---
+++ App/screens/Welcomescreen-LAPTOP-DIAKSNVA.js
@@ -0,0 +1,85 @@
+import React,{useState,useEffect} from 'react';
+import { StyleSheet, View, ImageBackground, Text } from 'react-native';
+import * as Font from 'expo-font';
+
+export default function AppTro() {
+  const [isFontLoaded, setIsFontLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFont() {
+      await Font.loadAsync({
+        'BalooPaaji2-Bold': require('../assets/fonts/BalooPaaji2-Bold.ttf'),
+        'BalooPaaji2-Medium': require('../assets/fonts/BalooPaaji2-Medium.ttf'),
+        
+       });
+      setIsFontLoaded(true);
+    }
+
+    loadFont();
+  }, []);
+  return (
+    <View style={styles.container}>
+      
+      <ImageBackground
+        source={require('../assets/image1.png')}
+        style={styles.backgroundImage}
+      >
+        <View style={styles.blueRectangle}>
+          <Text style={[styles.welcometitle , isFontLoaded && { fontFamily: 'BalooPaaji2-Bold' }]}>ਸਹਿਜ ਪਾਠ</Text>
+          <Text style={[styles.welcometext,isFontLoaded && { fontFamily: 'BalooPaaji2-Medium'}]}>Building the habit</Text>
+          <Text style={styles.welcometext}>of reading Gurbani</Text>
+
+        </View>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    width: '100%',
+    height: '100%', 
+    alignItems: 'center',
+    justifyContent: 'center',
+   
+  },
+  blueRectangle: {
+ flex:1,
+ width:'100%',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: '#0D2346',
+    opacity: 0.93,
+
+  },
+  welcometitle: {
+
+    fontSize: 50,
+    color: 'white',
+    textAlign: 'center',
+    marginTop:250,
+    
+ 
+
+
+
+  },
+  welcometext: {
+    
+    fontSize: 25,
+    color: 'white',
+    textAlign: 'center',
+
+
+  },
+});
