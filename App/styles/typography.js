13ffc8499b247be4071a0df261b1c76ddcec67f8
iKumarAbhinav <kumar@adaapt.ai>
2024-10-09 00:46:13 +0530

    Added two more typography

--- App/styles/typography.js
+++ App/styles/typography.js
@@ -16,6 +16,25 @@ export const typography = StyleSheet.create({
   black: {
     fontFamily: 'BrandonGrotesque-Black',
   },
+  balooRegular: {
+    fontFamily: 'BalooPaaji2-Regular',
+  },
+  balooMedium: {
+    fontFamily: 'BalooPaaji2-Medium',
+  },
+  balooBold: {
+    fontFamily: 'BalooPaaji2-Bold',
+  },
+  balooExtraBold: {
+    fontFamily: 'BalooPaaji2-ExtraBold',
+  },
+  balooSemiBold: {
+    fontFamily: 'BalooPaaji2-SemiBold',
+  },
+  recoletaRegular: {
+    fontFamily: 'Recoleta-Regular',
+  },
+  
 });
 
 
