9215d3ba920299a0b3776514d60cd17b3bd3c425
Abhishek singh <abhiramgarhia13@gmail.com>
2024-10-14 23:20:04 +0530

    updated welcomescreen

--- App/screens/Welcomescreen.js
+++ App/screens/Welcomescreen.js
@@ -1,27 +1,20 @@
 import React,{useState,useEffect} from 'react';
 import { StyleSheet, View, ImageBackground, Text, Button } from 'react-native';
 import * as Font from 'expo-font';
-// import {StackScreenProps} from "@react-navigation/stack"
-// import {RootStackParamList} from "../../App"
-// type homeprops=StackScreenProps<RootStackParamList,'Welcomescreen'>
 export default function AppTro() {
   const [isFontLoaded, setIsFontLoaded] = useState(false);
-
-  useEffect(() => {
+   useEffect(() => {
     async function loadFont() {
       await Font.loadAsync({
         'BalooPaaji2-Bold': require('../assets/fonts/BalooPaaji2-Bold.ttf'),
         'BalooPaaji2-Medium': require('../assets/fonts/BalooPaaji2-Medium.ttf'),
-        
-       });
+         });
       setIsFontLoaded(true);
     }
-
     loadFont();
   }, []);
   return (
     <View style={styles.container}>
-      
       <ImageBackground
         source={require('../assets/image1.png')}
         style={styles.backgroundImage}
@@ -30,8 +23,7 @@ export default function AppTro() {
           <Text style={[styles.welcometitle , isFontLoaded && { fontFamily: 'BalooPaaji2-Bold' }]}>ਸਹਿਜ ਪਾਠ</Text>
           <Text style={[styles.welcometext,isFontLoaded && { fontFamily: 'BalooPaaji2-Medium'}]}>Building the habit</Text>
           <Text style={styles.welcometext}>of reading Gurbani</Text>
-         
-        </View>
+          </View>
       </ImageBackground>
     </View>
   );
@@ -41,37 +33,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: '100%',
-    
-  
-  },
+    },
   backgroundImage: {
     flex: 1,
-    resizeMode: 'cover',
     width: '100%',
-    height: '100%', 
-  
-   
-  },
-  blueRectangle: {
- flex:1,
- width:'100%',
     
+   },
+  blueRectangle: {
+    flex:1,
+    width:'100%',
+    justifyContent:"center",
     backgroundColor: '#0D2346',
     opacity: 0.93,
-
-  },
+   },
   welcometitle: {
     fontSize: 50,
     color: 'white',
     textAlign: 'center',
-    marginTop:250,
+    
   },
   welcometext: {
-    
     fontSize: 25,
     color: 'white',
     textAlign: 'center',
-
-
   },
 });
