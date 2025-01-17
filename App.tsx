557f2b3d85b08eea43a7b66543b20e9c9b87edd3
simarjot <simarjot0032@gmail.com>
2025-01-17 16:33:30 +0530

    some code updation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,28 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 function App() {
   return (
     <>
       <SafeAreaView>
-        <Text style={{ fontSize: 25 }}>Sheaj-Path App</Text>
+        <View style={styleSheet.background}>
+          <Text style={styleSheet.text}>Khalis Foundation</Text>
+
+          <Text style={styleSheet.text}>Sheaj-Path-App</Text>
+        </View>
       </SafeAreaView>
     </>
   );
 }
-
+const styleSheet = StyleSheet.create({
+  background: {
+    backgroundColor: "black",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 25,
+  },
+});
 export default App;
