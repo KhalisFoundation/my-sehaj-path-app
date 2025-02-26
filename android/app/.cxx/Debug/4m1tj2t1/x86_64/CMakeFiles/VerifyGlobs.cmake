d475933e7cae03137ea21597e100c43f3a625de2
simarjot <simarjot0032@gmail.com>
2025-02-27 01:15:07 +0530

    Fix- name change SheajPath to SehajPath

--- android/app/.cxx/Debug/4m1tj2t1/x86_64/CMakeFiles/VerifyGlobs.cmake
+++ android/app/.cxx/Debug/4m1tj2t1/x86_64/CMakeFiles/VerifyGlobs.cmake
@@ -12,6 +12,44 @@ if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
   file(TOUCH_NOCREATE "/home/simar/my-sehaj-path-app/android/app/.cxx/Debug/4m1tj2t1/x86_64/CMakeFiles/cmake.verify_globs")
 endif()
 
+# rnsvg_SRCS at /home/simar/my-sehaj-path-app/node_modules/react-native-svg/android/src/main/jni/CMakeLists.txt:24 (file)
+file(GLOB NEW_GLOB LIST_DIRECTORIES true "/home/simar/my-sehaj-path-app/node_modules/react-native-svg/android/src/main/jni/*.cpp")
+set(OLD_GLOB
+  "/home/simar/my-sehaj-path-app/node_modules/react-native-svg/android/src/main/jni/rnsvg.cpp"
+  )
+if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
+  message("-- GLOB mismatch!")
+  file(TOUCH_NOCREATE "/home/simar/my-sehaj-path-app/android/app/.cxx/Debug/4m1tj2t1/x86_64/CMakeFiles/cmake.verify_globs")
+endif()
+
+# rnsvg_SRCS at /home/simar/my-sehaj-path-app/node_modules/react-native-svg/android/src/main/jni/CMakeLists.txt:24 (file)
+file(GLOB NEW_GLOB LIST_DIRECTORIES true "/home/simar/my-sehaj-path-app/node_modules/react-native-svg/android/src/main/jni/../../../../common/cpp/react/renderer/components/rnsvg/*.cpp")
+set(OLD_GLOB
+  "/home/simar/my-sehaj-path-app/node_modules/react-native-svg/android/src/main/jni/../../../../common/cpp/react/renderer/components/rnsvg/RNSVGImageShadowNode.cpp"
+  "/home/simar/my-sehaj-path-app/node_modules/react-native-svg/android/src/main/jni/../../../../common/cpp/react/renderer/components/rnsvg/RNSVGImageState.cpp"
+  "/home/simar/my-sehaj-path-app/node_modules/react-native-svg/android/src/main/jni/../../../../common/cpp/react/renderer/components/rnsvg/RNSVGLayoutableShadowNode.cpp"
+  "/home/simar/my-sehaj-path-app/node_modules/react-native-svg/android/src/main/jni/../../../../common/cpp/react/renderer/components/rnsvg/RNSVGShadowNodes.cpp"
+  )
+if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
+  message("-- GLOB mismatch!")
+  file(TOUCH_NOCREATE "/home/simar/my-sehaj-path-app/android/app/.cxx/Debug/4m1tj2t1/x86_64/CMakeFiles/cmake.verify_globs")
+endif()
+
+# rnsvg_codegen_SRCS at /home/simar/my-sehaj-path-app/node_modules/react-native-svg/android/src/main/jni/CMakeLists.txt:25 (file)
+file(GLOB NEW_GLOB LIST_DIRECTORIES true "/home/simar/my-sehaj-path-app/node_modules/react-native-svg/android/src/main/jni/../../../build/generated/source/codegen/jni/react/renderer/components/rnsvg/*cpp")
+set(OLD_GLOB
+  "/home/simar/my-sehaj-path-app/node_modules/react-native-svg/android/src/main/jni/../../../build/generated/source/codegen/jni/react/renderer/components/rnsvg/ComponentDescriptors.cpp"
+  "/home/simar/my-sehaj-path-app/node_modules/react-native-svg/android/src/main/jni/../../../build/generated/source/codegen/jni/react/renderer/components/rnsvg/EventEmitters.cpp"
+  "/home/simar/my-sehaj-path-app/node_modules/react-native-svg/android/src/main/jni/../../../build/generated/source/codegen/jni/react/renderer/components/rnsvg/Props.cpp"
+  "/home/simar/my-sehaj-path-app/node_modules/react-native-svg/android/src/main/jni/../../../build/generated/source/codegen/jni/react/renderer/components/rnsvg/ShadowNodes.cpp"
+  "/home/simar/my-sehaj-path-app/node_modules/react-native-svg/android/src/main/jni/../../../build/generated/source/codegen/jni/react/renderer/components/rnsvg/States.cpp"
+  "/home/simar/my-sehaj-path-app/node_modules/react-native-svg/android/src/main/jni/../../../build/generated/source/codegen/jni/react/renderer/components/rnsvg/rnsvgJSI-generated.cpp"
+  )
+if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
+  message("-- GLOB mismatch!")
+  file(TOUCH_NOCREATE "/home/simar/my-sehaj-path-app/android/app/.cxx/Debug/4m1tj2t1/x86_64/CMakeFiles/cmake.verify_globs")
+endif()
+
 # override_cpp_SRC at /home/simar/my-sehaj-path-app/node_modules/react-native/ReactAndroid/cmake-utils/ReactNative-application.cmake:42 (file)
 # input_SRC at /home/simar/my-sehaj-path-app/node_modules/react-native/ReactAndroid/cmake-utils/ReactNative-application.cmake:47 (file)
 file(GLOB NEW_GLOB LIST_DIRECTORIES true "/home/simar/my-sehaj-path-app/node_modules/react-native/ReactAndroid/cmake-utils/default-app-setup/*.cpp")
