c584b548f57f3f59dc4f76ebc7f04a7c341e5d6f
simarjot <simarjot0032@gmail.com>
2025-02-27 23:32:15 +0530

    fix: ios/sheajpath->sehajpath renamed

--- ios/SehajPathAppTests/SehajPathAppTests.m
+++ ios/SehajPathAppTests/SehajPathAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SheajPathAppTests : XCTestCase
+@interface SehajPathAppTests : XCTestCase
 
 @end
 
-@implementation SheajPathAppTests
+@implementation SehajPathAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
