eda07df7763e3aba620c7349666b2b66c1634366
simarjot <simarjot0032@gmail.com>
2025-04-08 01:01:01 +0530

    Animation for SplashScreen

--- screens/SplashScreen.tsx
+++ screens/SplashScreen.tsx
@@ -1,27 +1,37 @@
-import { View, Text, ImageBackground } from "react-native";
-import React from "react";
+import { View, Text, ImageBackground, Animated } from "react-native";
+import React, { useEffect, useRef } from "react";
 import { SplashScreenStyles } from "../styles/SplashScreenStyles";
 import { Constants } from "../constants";
 
 export default function SplashScreen() {
+  const fadeOut = useRef(new Animated.Value(1)).current;
+  useEffect(() => {
+    Animated.timing(fadeOut, {
+      toValue: 0,
+      duration: 1500,
+      useNativeDriver: true,
+    }).start();
+  }, []);
   return (
     <>
-      <ImageBackground
-        source={require("../assets/Images/SplashScreenBg.png")}
-        style={SplashScreenStyles.backgroundImage}
-      >
-        <View style={SplashScreenStyles.overlayBg}>
-          <Text style={SplashScreenStyles.mainHeadline}>
-            {Constants.SEHAJ_PATH}
-          </Text>
-          <Text style={SplashScreenStyles.tagline}>
-            {Constants.BUILDING_THE_HABIT}
-          </Text>
-          <Text style={SplashScreenStyles.tagline}>
-            {Constants.OF_READING_GURBANI}
-          </Text>
-        </View>
-      </ImageBackground>
+      <Animated.View style={{ opacity: fadeOut }}>
+        <ImageBackground
+          source={require("../assets/Images/SplashScreenBg.png")}
+          style={SplashScreenStyles.backgroundImage}
+        >
+          <View style={SplashScreenStyles.overlayBg}>
+            <Text style={SplashScreenStyles.mainHeadline}>
+              {Constants.SEHAJ_PATH}
+            </Text>
+            <Text style={SplashScreenStyles.tagline}>
+              {Constants.BUILDING_THE_HABIT}
+            </Text>
+            <Text style={SplashScreenStyles.tagline}>
+              {Constants.OF_READING_GURBANI}
+            </Text>
+          </View>
+        </ImageBackground>
+      </Animated.View>
     </>
   );
 }
